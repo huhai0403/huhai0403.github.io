48844fc4950608470b164b09da3a4ad6f6cc2be8
huhai0403 <13098898233@163.com>
2019-04-30 16:43:40 +0800

    sikaogen

---
+++ sikaogen/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+   //底部tab切换
+   $('.tab .weui-tabbar__item').click(function () {
+       $(this).siblings().removeClass('active');
+       $(this).addClass('active');
+   });
+   /*首页跳转*/
+   $('.menu-list li').click(function () {
+       window.location.href = 'courseList.html'
+   });
+   //首页搜索
+    $('.search-wrap img').click(function () {
+        window.location.href = 'searchList.html'
+    });
+   /*课程列表选择分类*/
+   $('.courseList .top-nav li,.courseList .mid-nav li').click(function () {
+       $(this).siblings().removeClass('active');
+       $(this).addClass('active');
+   });
+});
